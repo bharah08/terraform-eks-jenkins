9767e6fe063edf9f7bad82705e019692c7afa44b
bharah08 <cxx7148@gmail.com>
2024-06-14 13:41:52 +0530

    updated

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,26 @@
-resource "aws_subnet" "public-1" {
+resource "aws_subnet" "private-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "private-sub-1"
+  }
+}
+resource "aws_subnet" "private-2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "private-sub-2"
+  }
+}
+resource "aws_subnet" "public-1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
@@ -8,10 +28,9 @@ resource "aws_subnet" "public-1" {
     Name = "public-sub-1"
   }
 }
-
 resource "aws_subnet" "public-2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.4.0/24"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 
